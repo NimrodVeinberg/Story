3e91f40ce50c4bcd480bc18870fa176250ddc34a
Nimrod <nimrod.v@arpeely.com>
2022-05-04 17:59:48 +0300

    Add story configuration with progress bar

--- story.js
+++ story.js
@@ -1,10 +1,7 @@
-//document.getElementById("carrousel").src = "./";
-
 async function getPokemonData() {
   return await fetch("http://localhost:8081/pokemon")
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       return data;
     });
 }
@@ -13,7 +10,6 @@ function createDiv(className, parentElement, textContent) {
   const newDiv = document.createElement("div");
 
   if (className) {
-    // debugger;
     for (let i = 0; i < className.length; i++) {
       newDiv.classList.add(`${className[i]}`);
     }
@@ -26,14 +22,80 @@ function createDiv(className, parentElement, textContent) {
   return newDiv;
 }
 
-// Time bar
-function createBarElement() {
-  createDiv(["proggress-bar"]);
+// Progress bar
+function createBarElement(picturesNumber) {
+  const progressBorder = createDiv(["progress-border"]);
+  for (let i = 0; i < picturesNumber.length; i++) {
+    createDiv("");
+  }
+  createDiv(["progress-bar"], progressBorder);
+}
+
+// change the progress bar
+function changeProgressBar() {
+  bar = document.querySelector(".progress-bar");
+  let width = 0;
+  function progressBarAnimation() {
+    width++;
+    if (width > 100) {
+      scrollStory();
+      width = 0;
+    }
+    bar.style.width = `${width}%`;
+  }
+  setInterval(progressBarAnimation, 10);
+}
+let moveLeft = 0;
+
+function scrollStory() {
+  // let cards = document.querySelector(".cards");
+  // let bar = document.querySelector(".card-container");
+  let screenWidth = screen.width;
+  console.log(moveLeft);
+  moveLeft += screenWidth;
+  // debugger;
+  window.scroll({
+    left: moveLeft,
+    behavior: "smooth",
+  });
+  // window.scrollTo(0, document.body.scrollWidth);
+  // cards.scrollWidth += 10;
+  // cards.scrollRight;
+}
+
+function addEventListener() {
+  let cards = document.querySelector(".cards");
+  cards.addEventListener("click", stopScroll);
+  cards.addEventListener("mouseup", mouseUp);
+  cards.addEventListener("dragend", dragOver);
+}
+
+// let cardPos;
+
+function stopScroll() {
+  console.log("stopScroll");
+  cards = document.querySelector(".cards");
+  // cardPos = cards.getBoundingClientRect();
+  // console.log(cardPos);
+}
+
+function mouseUp() {
+  console.log("mouseUp");
+  const cards = document.querySelector(".cards");
+  // cards.style.top = cardPos.top;
+  // cards.style.x = cardPos.x;
+  // cards.style.y = cardPos.y;
+}
+
+function dragOver() {
+  console.log("dragOver");
+  const cards = document.querySelector(".cards");
+  cards.style.align = "start";
 }
 
 function CreateCardElement(pokemonObjData, cardsDiv) {
-  // Should have a parent of div Cards and not body
-  const cardContainerDiv = createDiv(["card-container"]);
+  // debugger;
+  const cardContainerDiv = createDiv(["card-container"], cardsDiv);
   createDiv(["top-card-info"], cardContainerDiv, `Hp ${pokemonObjData.hp}`);
   const img = document.createElement("img");
   img.setAttribute("src", pokemonObjData.img);
@@ -57,7 +119,6 @@ function CreateCardElement(pokemonObjData, cardsDiv) {
   ];
   dataProperties.forEach((element, i) => {
     const propertiesDiv = createDiv(["properties"], pokemonProperties);
-    console.log(element);
     createDiv(
       [`properties-${i}`, `font-properties`],
       propertiesDiv,
@@ -69,10 +130,14 @@ function CreateCardElement(pokemonObjData, cardsDiv) {
 
 (async function createDomPage() {
   const allCardsDiv = createDiv(["cards"]);
-  createBarElement();
-  const data = await getPokemonData();
-  const dataPokemons = data.data;
-  console.log(dataPokemons);
+  // debugger;
+  // var cardsWidth = window.innerWidth * 14;
+  // allCardsDiv.style.width = `${cardsWidth}px`;
+  const { data } = await getPokemonData();
+  const dataPokemons = data;
+  createBarElement(dataPokemons.length);
+  changeProgressBar();
+  addEventListener();
   for (let i = 0; i < dataPokemons.length; i++) {
     CreateCardElement(dataPokemons[i], allCardsDiv);
   }
